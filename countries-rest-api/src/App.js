3867646c13f4bda84acf4f74c56744b794b4072c
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-08-03 07:23:24 +0100

    styling error

--- countries-rest-api/src/App.js
+++ countries-rest-api/src/App.js
@@ -2,23 +2,22 @@ import React, { useState, useEffect } from "react";
 import Card from "./components/Card";
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     fetch(`https://restcountries.com/v2/all`)
       .then((res) => res.json())
       .then((data) => {
-        setImages(data);
+        setCountries(data);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="bg-DarkModeBackground font-sans md:px-[5%]">
-      <div className="md:grid md:grid-cols-4 md:gap-10 flex flex-col px-[10%] ">
-        {images.map(image => (
-          <Card key={image.id} image={image}/>
+      <div className="md:grid md:grid-cols-4 md:gap-10 flex flex-col px-[10%] md:px-[2%] ">
+        {countries.map(country => (
+          <Card key={country.id} country_data={country}/>
         ))}
       </div>
     </div>
--- countries-rest-api/src/components/Card.js
+++ countries-rest-api/src/components/Card.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-const Card = ({ image }) => {
+const Card = ({ country_data }) => {
   return (
     <div className="bg-DarkBlue max-w-sm rounded-lg container mx-auto mt-20">
-      <img src={image.flags.svg} className="rounded-tr-lg rounded-tl-lg h-[200px] md:h-[150px]  w-full" />
+      <img src={country_data.flags.svg} className="rounded-tr-lg rounded-tl-lg h-[200px] md:h-[150px]  w-full" />
 
       <div className="mx-6 pb-10 mt-6">
-        <h3 className="font-bold text-White pb-2">{image.name}</h3>
+        <h3 className="font-bold text-White pb-2">{country_data.name}</h3>
         <ol className="text-LightModeInput">
           <li>
             <span className="text-White">Population: </span>
-            {image.population}
+            {country_data.population}
           </li>
           <li>
             <span className="text-White">Region: </span>
-            {image.region}
+            {country_data.region}
           </li>
           <li>
             <span className="text-White">Capital: </span>
-            {image.capital}
+            {country_data.capital}
           </li>
         </ol>
       </div>
