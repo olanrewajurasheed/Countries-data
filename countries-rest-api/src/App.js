355bc5072a3ef9a90286f83cbddb7a4ec9ffc6c9
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-08-03 07:19:05 +0100

    data fetching

--- countries-rest-api/src/App.js
+++ countries-rest-api/src/App.js
@@ -4,20 +4,23 @@ import Card from "./components/Card";
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState('');
 
-  useEffect( () => {
+  useEffect(() => {
     fetch(`https://restcountries.com/v2/all`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-  
-  }, [])
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="bg-DarkModeBackground h-screen font-sans">
-      <div>.</div>
-      <Card/>
-      
+    <div className="bg-DarkModeBackground font-sans md:px-[5%]">
+      <div className="md:grid md:grid-cols-4 md:gap-10 flex flex-col px-[10%] ">
+        {images.map(image => (
+          <Card key={image.id} image={image}/>
+        ))}
+      </div>
     </div>
   );
 }
--- countries-rest-api/src/components/Card.js
+++ countries-rest-api/src/components/Card.js
@@ -1,29 +1,29 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ image }) => {
+  return (
+    <div className="bg-DarkBlue max-w-sm rounded-lg container mx-auto mt-20">
+      <img src={image.flags.svg} className="rounded-tr-lg rounded-tl-lg h-[200px] md:h-[150px]  w-full" />
 
-    return (
-        <div className="bg-DarkBlue max-w-sm rounded-lg container mx-auto mt-20">
-            <img src="https://source.unsplash.com/random" className="rounded-tr-lg rounded-tl-lg"/>
-            <div className="mx-6 pb-10 mt-6">
-                <h3 className="font-bold text-White pb-2">Nigeria</h3>
-                <ol className="text-LightModeInput">
-                    <li>
-                        <span className="text-White">Population: </span>
-                        204,450,000
-                    </li>
-                    <li>
-                        <span className="text-White">Region: </span>
-                        Africa
-                    </li>
-                    <li>
-                        <span className="text-White">Capital: </span>
-                        Abuja
-                    </li>
-                </ol>
-            </div>
-        </div>
-    )
+      <div className="mx-6 pb-10 mt-6">
+        <h3 className="font-bold text-White pb-2">{image.name}</h3>
+        <ol className="text-LightModeInput">
+          <li>
+            <span className="text-White">Population: </span>
+            {image.population}
+          </li>
+          <li>
+            <span className="text-White">Region: </span>
+            {image.region}
+          </li>
+          <li>
+            <span className="text-White">Capital: </span>
+            {image.capital}
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
