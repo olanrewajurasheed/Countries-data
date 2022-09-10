93104c804c3a935f7eb56c38e23f813f3819118d
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-09-10 19:01:28 +0100

    filtering setup

--- src/App.js
+++ src/App.js
@@ -15,26 +15,30 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
+  const [filteredRegion, setFilteredRegion] = useState("");
+  const filteredChangeHandler = (selectedRegion) => {
+    setFilteredRegion(selectedRegion);
+  };
   return (
     <div className="bg-DarkModeBackground font-sans ">
       <div className="bg-DarkBlue flex justify-between font-semibold text-White py-6 md:px-[5%] px-[5%]">
         {/* Header section  */}
         <h4 className="font-bold text-lg">Where in the world ?</h4>
         <div class="flex gap-4 ">
-          <img src={moonIcon}
-          className="h-[20px] w-[20px]"/>Dark Mode
+          <img src={moonIcon} className="h-[20px] w-[20px]" />
+          Dark Mode
         </div>
       </div>
 
       {/* Search and Filter Section  */}
       <div className="flex flex-col md:flex-row mx-[5%] mt-10 justify-between gap-10">
-        <SearchForm/>
-        <FilterRegion />
+        <SearchForm />
+        <FilterRegion selected={filteredRegion} onChangeFilter={filteredChangeHandler}/>
       </div>
 
       <div className="md:grid md:grid-cols-4 md:gap-10 flex flex-col px-[1%] md:px-[5%]">
-        {countries.map(country => (
-          <Card key={country.id} country_data={country}/>
+        {countries.map((country) => (
+          <Card key={country.id} country_data={country} />
         ))}
       </div>
     </div>
